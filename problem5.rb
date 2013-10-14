d09c3ccce20091def41e3af274e5f0f58ad4d3aa
Jeff Johnson <jeffjohnson9046hotmail.com>
2013-10-14 15:27:57 -0700

    Corrected recursive call - use .unshift to append the head to the accumulator (rev_ary in this case).

--- problem5.rb
+++ problem5.rb
@@ -5,15 +5,12 @@
 # 	scala> reverse(List(1, 1, 2, 3, 5, 8))
 # 	res0: List[Int] = List(8, 5, 3, 2, 1, 1)
 ###############################################################################
-def reverse_list(ary, rev_ary)
+def reverse_list(ary, rev_ary = [])
 	return rev_ary if ary.empty?
 
 	head, *tail = ary
-	reverse_list(tail, rev_ary)
-
-	rev_ary << head
+	reverse_list(tail, rev_ary.unshift(head))
 end
 
 a = [1, 1, 2, 3, 5, 8]
-ra = []
-puts "reverse #{reverse_list(a, ra)}"
+puts "reverse_list #{reverse_list(a)}"
