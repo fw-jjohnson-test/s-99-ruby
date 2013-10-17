2acf61f46500ba2cea2b61b45d4cd097f4b14c79
Jeff Johnson <jeffjohnson9046hotmail.com>
2013-10-17 08:31:08 -0700

    Added comments to identify what previous problem files provide.

--- problem10.rb
+++ problem10.rb
@@ -8,9 +8,9 @@
 # 	scala> encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
 # 	res0: List[(Int, Symbol)] = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
 ###############################################################################
-require_relative 'problem3'
-require_relative 'problem4'
-require_relative 'problem9'
+require_relative 'problem3' # Find the Kth element of a list.
+require_relative 'problem4' # Find the number of elements of a list.
+require_relative 'problem9' # Pack consecutive duplicates of list elements into sublists.
 
 def encode(ary, acc = [])
 	head, *tail = ary
