2b3effb4a4d905c16c73ea05d8f060b7f5289519
Jeff Johnson <jeffjohnson9046@hotmail.com>
2014-07-03 13:22:15 -0700

    Removed code that was already completed in problem 5; added 'require_relative' to include problem 5's code.

--- problem6.rb
+++ problem6.rb
@@ -5,13 +5,7 @@
 # 	scala> isPalindrome(List(1, 2, 3, 2, 1))
 # 	res0: Boolean = true
 ###############################################################################
-# From problem 5
-def reverse_list(ary, rev_ary = [])
-	return rev_ary if ary.empty?
-
-	head, *tail = ary
-	reverse_list(tail, rev_ary.unshift(head))
-end
+require_relative 'problem5'
 
 def is_palindrome(ary)
 	ary == reverse_list(ary)
