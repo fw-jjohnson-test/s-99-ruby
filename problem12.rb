be6dcc88643007772508ed9ee19fc0b1a44fbfd8
Jeff Johnson <jeffjohnson9046@hotmail.com>
2014-08-21 14:30:45 -0700

    Solved probelm 12.

---
+++ problem12.rb
@@ -0,0 +1,30 @@
+###############################################################################
+# Decode a run-length encoded list.
+# Given a run-length code list generated as specified in problem P10, construct 
+# its uncompressed version.
+#
+# Example:
+# 	scala> decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+#   res0: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+###############################################################################
+require_relative 'problem10'
+
+def decode(ary, acc = [])
+	head, *tail = ary
+	return acc if head.nil?
+
+	if head.is_a?(Array)
+		expanded = []
+		head.first.times { expanded << head.last }
+		acc << expanded
+	else
+		acc << head
+	end
+
+	decode(tail, acc)
+end
+
+result = []
+a = [[4, "a"], "b", [2, "c"], [2, "a"], "d", [4, "e"]]
+decode(a, result)
+puts "#{result}" # => [["a", "a", "a", "a"], "b", ["c", "c"], ["a", "a"], "d", ["e", "e", "e", "e"]]
