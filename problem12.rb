f70a304a784efce04d526a2b4728d8a812400f6b
Jeff Johnson <jeffjohnson9046@hotmail.com>
2014-08-21 22:39:41 -0700

    Changed to match output requirements.

--- problem12.rb
+++ problem12.rb
@@ -14,9 +14,7 @@ def decode(ary, acc = [])
 	return acc if head.nil?
 
 	if head.is_a?(Array)
-		expanded = []
-		head.first.times { expanded << head.last }
-		acc << expanded
+		head.first.times { acc << head.last }
 	else
 		acc << head
 	end
@@ -27,4 +25,4 @@ end
 result = []
 a = [[4, "a"], "b", [2, "c"], [2, "a"], "d", [4, "e"]]
 decode(a, result)
-puts "#{result}" # => [["a", "a", "a", "a"], "b", ["c", "c"], ["a", "a"], "d", ["e", "e", "e", "e"]]
+puts "#{result}" # => ["a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e"]
