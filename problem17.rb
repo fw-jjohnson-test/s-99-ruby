47ad256841bcbe3a94be499daa3e1dd0504cdfa7
Jeff Johnson <jeffjohnson9046@hotmail.com>
2014-09-18 10:14:20 -0700

    Converted tabs to spaces.

--- problem17.rb
+++ problem17.rb
@@ -2,17 +2,17 @@
 # The length of the first part is given. Use a Tuple for your result.
 #
 # Example:
-# 	scala> split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
-# 	res0: (List[Symbol], List[Symbol]) = (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+#   scala> split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+#   res0: (List[Symbol], List[Symbol]) = (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
 ###############################################################################
 def split(list, n)
-	raise ArgumentError.new("list cannot be empty") if list.empty?
-	raise ArgumentError.new("n must be greater than 0 and less than the length of the list.") if n == 0 or n >= list.length
+    raise ArgumentError.new("list cannot be empty") if list.empty?
+    raise ArgumentError.new("n must be greater than 0 and less than the length of the list.") if n == 0 or n >= list.length
 
-	list.reduce({ :left => [], :right => []}) do |hash, item| 
-		list.index(item) < n ? hash[:left] << item : hash[:right] << item
-		hash
-	end
+    list.reduce({ :left => [], :right => []}) do |hash, item| 
+        list.index(item) < n ? hash[:left] << item : hash[:right] << item
+        hash
+    end
 end
 
 list = 'a'.upto('k').to_a
@@ -24,10 +24,10 @@ puts split(list, 6)
 # puts split(list, 0) # test "invalid index" error
 
 def split_duh(list, n)
-	raise ArgumentError.new("list cannot be empty") if list.empty?
-	raise ArgumentError.new("n must be greater than 0 and less than the length of the list.") if n == 0 or n >= list.length
+    raise ArgumentError.new("list cannot be empty") if list.empty?
+    raise ArgumentError.new("n must be greater than 0 and less than the length of the list.") if n == 0 or n >= list.length
 
-	{ :left => list[0...n], :right => list[n..-1] }
+    { :left => list[0...n], :right => list[n..-1] }
 end
 
 puts split_duh(list, 3)
--- problem18.rb
+++ problem18.rb
@@ -4,17 +4,17 @@
 # original list. Start counting the elements with 0.
 #
 # Example:
-# 	scala> slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
-# 	res0: List[Symbol] = List('d, 'e, 'f, 'g)
+#   scala> slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+#   res0: List[Symbol] = List('d, 'e, 'f, 'g)
 ###############################################################################
 
 def slice_list(list, start, finish)
-	raise ArgumentError.new("list cannot be empty.") if list.empty?
-	raise ArgumentError.new("start must be greater than 0 and less than the length of the list.") if start == 0 or start >= list.length
-	raise ArgumentError.new("finish must be greater than start and less than the length of the list.") if finish <= start or finish > list.length
+    raise ArgumentError.new("list cannot be empty.") if list.empty?
+    raise ArgumentError.new("start must be greater than 0 and less than the length of the list.") if start == 0 or start >= list.length
+    raise ArgumentError.new("finish must be greater than start and less than the length of the list.") if finish <= start or finish > list.length
 
-	list.map { |item| item if list.index(item) >= start and list.index(item) < finish  }
-		.compact
+    list.map { |item| item if list.index(item) >= start and list.index(item) < finish  }
+        .compact
 end
 
 list = 'a'.upto('k').to_a
@@ -22,7 +22,7 @@ list = 'a'.upto('k').to_a
 puts slice_list(list, 3, 7)
 
 def slice_list_duh(list, start, finish)
-	list[start...finish]
+    list[start...finish]
 end
 
 puts slice_list_duh(list, 3, 7).to_s
